2e959736e87dcf296165ad4e051fbcd61d25c200
William Harris <wharris@upscalews.com>
2014-09-15 18:53:56 -0700

    common follwers

--- app/controllers/twitter_accounts_controller.rb
+++ app/controllers/twitter_accounts_controller.rb
@@ -5,7 +5,30 @@ class TwitterAccountsController < ApplicationController
     @tweets = []
 
     if @account_name.present?
-      @tweets = TwitterAccount.client.user_timeline(@account_name)
+      @tweets = TwitterAccount.client.user_timeline(@account_name.strip)
     end
   end
+
+  def compare
+    @first_account_name = params[:first_account_name]
+    @second_account_name = params[:second_account_name]
+    
+    @fa_fol_ids = []
+    @sa_fol_ids = []
+
+    if @first_account_name.present? and @second_account_name.present?
+      @fa_fol_ids = TwitterAccount.client.follower_ids(@first_account_name.strip).attrs[:ids]
+      @sa_fol_ids = TwitterAccount.client.follower_ids(@second_account_name.strip).attrs[:ids]
+    end
+
+    @common_follower_ids = (@fa_fol_ids & @sa_fol_ids)
+
+    # cut to 30 so we dont make alot of api calls
+    @common_followers = @common_follower_ids.first(30).map do |fid|
+      user = TwitterAccount.client.user(fid)
+      "#{user.screen_name} - #{user.name}"
+    end
+
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resource :twitter_accounts
+  get 'twitter_accounts/compare', to: 'twitter_accounts#compare'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
